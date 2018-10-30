2c83f6de456401ef36c67672909f99f0f21c44ae
saikumar7084 <boddu.saikumar1994@gmail.com>
2018-10-30 10:45:46 +0530

    Sign Up Changes

--- app/src/main/java/com/webingate/paysmartbusinessapp/activity/businessapp/customerSignUpActivity.java
+++ app/src/main/java/com/webingate/paysmartbusinessapp/activity/businessapp/customerSignUpActivity.java
@@ -1,19 +1,53 @@
 package com.webingate.paysmartbusinessapp.activity.businessapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.JsonObject;
 import com.webingate.paysmartbusinessapp.R;
+import com.webingate.paysmartbusinessapp.driverapplication.Deo.RegisterDriverResponse;
+import com.webingate.paysmartbusinessapp.driverapplication.RegisterActivity;
+import com.webingate.paysmartbusinessapp.driverapplication.VerificationActivity;
 import com.webingate.paysmartbusinessapp.utils.Utils;
+
+import java.util.List;
+
+import butterknife.BindView;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class customerSignUpActivity extends AppCompatActivity {
 
-    TextView forgotTextView, signUpTextView;
+    public static final String MyPREFERENCES = "MyPrefs";
+    public static final String Name = "nameKey";
+    public static final String Phone = "phoneKey";
+    public static final String Email = "emailKey";
+    public static final String Password = "passwordkey";
+    public static final String Mobileotp = "mobileotpkey";
+    public static final String Emailotp = "emailotpkey";
+    public static final String DRIVERID = "driverid";
+    public static final String VEHICLEID = "vehicleid";
+    Toast toast;
+    @BindView(R.id.registerButton)
     Button registerButton;
+    @BindView(R.id.s_username)
+    EditText S_username;
+    @BindView(R.id.s_password)
+    EditText S_password;
+    @BindView(R.id.s_email)
+    EditText S_email;
+    TextView forgotTextView, signUpTextView;
+   //Button registerButton;
     ImageView bgImageView;
 
     @Override
@@ -35,6 +69,10 @@ public class customerSignUpActivity extends AppCompatActivity {
         signUpTextView = findViewById(R.id.signuptTextView);
 
         registerButton = findViewById(R.id.registerButton);
+        S_username=findViewById(R.id.s_username);
+        S_password=findViewById(R.id.s_password);
+        S_username=findViewById(R.id.s_email);
+
         bgImageView = findViewById(R.id.bgImageView);
     }
 
@@ -62,12 +100,75 @@ public class customerSignUpActivity extends AppCompatActivity {
         registerButton.setOnClickListener(view -> {
             //Toast.makeText(getApplicationContext(), "Clicked Register.", Toast.LENGTH_SHORT).show();
 
-            Intent intent = new Intent(this, customerEOTPVerificationActivity.class);
-            startActivity(intent);
+            JsonObject object = new JsonObject();
+            object.addProperty("flag", "I");
+            object.addProperty("Firstname", "hnmisv");
+            object.addProperty("lastname", "sv");
+            object.addProperty("AuthTypeId", "2");
+            object.addProperty("Password", "123");
+            object.addProperty("Mobilenumber", "7893890990");
+            object.addProperty("Email", "hnmisv@gmail.com");
+            RegisterDriver(object);
+//            Intent intent = new Intent(this, customerEOTPVerificationActivity.class);
+//            startActivity(intent);
         });
 
     }
+    public void RegisterDriver(JsonObject jsonObject){
+
+        //StartDialogue();
+        com.webingate.paysmartbusinessapp.driverapplication.Utils.DataPrepare.get(customerSignUpActivity.this).getrestadapter()
+                .RegisterDriver(jsonObject)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<RegisterDriverResponse>>() {
+                    @Override
+                    public void onCompleted() {
+                        DisplayToast("Successfully onCompleted");
+                        //StopDialogue();
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        try {
+                            DisplayToast("Successfully onError");
+                            //DisplayToast("Unable to Register");
+                            //StopDialogue();
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onNext(List<RegisterDriverResponse> responseList) {
+                        DisplayToast("Successfully onNext");
+                        RegisterDriverResponse response=responseList.get(0);
+                        SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putString(Name, response.getName());
+                        editor.putString(Phone, response.getPMobNo());
+                        editor.putString(Email, response.getEmail());
+                        editor.putString(Password, response.getPassword());
+                        editor.putString(Mobileotp, response.getMobileotp());
+                        editor.putString(Emailotp, response.getEmailotp());
+                        editor.putString(DRIVERID, response.getDriverId());
+                        editor.putString(VEHICLEID, response.getVehicleId());
+                        editor.commit();
+                        startActivity(new Intent(customerSignUpActivity.this, customerEOTPVerificationActivity.class));
+                        finish();
 
 
+                    }
+                });
+    }
+    public void DisplayToast(String text){
+        if(toast!=null){
+            toast.cancel();
+            toast=null;
+
+        }
+        toast= Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
     //endregion
 }
--- app/src/main/java/com/webingate/paysmartbusinessapp/activity/businessapp/login_activity.java
+++ app/src/main/java/com/webingate/paysmartbusinessapp/activity/businessapp/login_activity.java
@@ -2,9 +2,11 @@ package com.webingate.paysmartbusinessapp.activity.businessapp;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,16 +20,43 @@ import android.widget.Toast;
 import com.google.gson.JsonObject;
 import com.webingate.paysmartbusinessapp.R;
 import com.webingate.paysmartbusinessapp.adapter.uicollection.CustomSpinnerAdapter;
+import com.webingate.paysmartbusinessapp.driverapplication.ApplicationConstants;
+import com.webingate.paysmartbusinessapp.driverapplication.Deo.DriverValidateCredentialsResponse;
+
+import com.webingate.paysmartbusinessapp.driverapplication.Dialog.ProgressDialog;
 import com.webingate.paysmartbusinessapp.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindView;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class login_activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
+    public static final String MyPREFERENCES = "MyPrefs";
+    public static final String Name = "nameKey";
+    public static final String Phone = "phoneKey";
+    public static final String Email = "emailKey";
+    public static final String Password = "passwordkey";
+    public static final String Mobileotp = "mobileotpkey";
+    public static final String Emailotp = "emailotpkey";
+    public static final String Dateofbirth = "dateofbirth";
+    public static final String Gender = "gender";
+    public static final String Paymenttype = "paymenttype";
+    public static final String Profilepic = "profilepic";
+    public static final String DRIVERID = "driverid";
+    public static final String VEHICLEID = "vehicleid";
+
+    private String response;
+    Toast toast;
+    ProgressDialog dialog ;
     TextView forgotTextView, signUpTextView;
+
+
     @BindView(R.id.loginButton)
     Button loginButton;
     CardView facebookCardView, twitterCardView;
@@ -39,6 +68,10 @@ public class login_activity extends AppCompatActivity implements AdapterView.OnI
     int loginasOption = -1;
     @BindView(R.id.input_mobile)
     EditText mobileNo;
+
+    @BindView(R.id.textPassword)
+    EditText textPassword;
+
 
     String[] fruits = {"Driver", "Fleet owner", "Ticket Agent", "Brand ambassador","Business Owner"};
     int[] icons = {R.drawable.baseline_person_outline_black_24, R.drawable.baseline_person_outline_black_24, R.drawable.baseline_person_outline_black_24
@@ -75,6 +108,9 @@ public class login_activity extends AppCompatActivity implements AdapterView.OnI
         signUpTextView = findViewById(R.id.signuptTextView);
 
         loginButton = findViewById(R.id.loginButton);
+        mobileNo = findViewById(R.id.mobileNo);
+        textPassword = findViewById(R.id.textPassword);
+
 //        facebookCardView = findViewById(R.id.facebookCardView);
 //        twitterCardView = findViewById(R.id.twitterCardView);
         bgImageView = findViewById(R.id.bgImageView);
@@ -92,74 +128,26 @@ public class login_activity extends AppCompatActivity implements AdapterView.OnI
             startActivity(intent);
         });
 
-        signUpTextView.setOnClickListener(view -> {
+        signUpTextView.setOnClickListener((View view) -> {
            // Toast.makeText(getApplicationContext(), "Clicked Sign Up.", Toast.LENGTH_SHORT).show();
 
+            //Intent intent = new Intent(this, customerSignUpActivity.class);
             Intent intent = new Intent(this, customerSignUpActivity.class);
             startActivity(intent);
         });
 
         loginButton.setOnClickListener(view -> {
 
-//            if (mobileNo.getText().toString().matches("") || password.getText().toString().matches("")) {
+//            if (mobileNo.getText().toString().matches("") || textPassword.getText().toString().matches("")) {
 //                Toast.makeText(getApplicationContext(), "Please Enter details", Toast.LENGTH_SHORT).show();
 //            } else {
 //                JsonObject jsonObject = new JsonObject();
 //                jsonObject.addProperty("Mobilenumber", mobileNo.getText().toString());
-//                jsonObject.addProperty("Password", password.getText().toString());
+//                jsonObject.addProperty("Password", textPassword.getText().toString());
 //                DriverLogin(jsonObject);
 //            }
+            GoToDashboard();
             //Toast.makeText(getApplicationContext(), "Clicked Login.", Toast.LENGTH_SHORT).show();
-           switch (this.loginasOption){
-               case 0:
-                   Toast.makeText(getApplicationContext(), "Clicked option 0.", Toast.LENGTH_SHORT).show();
-
-
-                   Intent intent = new Intent(this, businessappDriverDashboardActivity.class);
-                   // EditText editText = (EditText) findViewById(R.id.editText);
-                   // String message = editText.getText().toString();
-                   //  intent.putExtra(EXTRA_MESSAGE, message);
-                   startActivity(intent);
-
-                   break;
-               case 1:
-                   Toast.makeText(getApplicationContext(), "Clicked option 1.", Toast.LENGTH_SHORT).show();
-                    intent = new Intent(this, businessappFleetownerDashboardActivity.class);
-                   // EditText editText = (EditText) findViewById(R.id.editText);
-                   // String message = editText.getText().toString();
-                   //  intent.putExtra(EXTRA_MESSAGE, message);
-                   startActivity(intent);
-                   break;
-
-               case 2:
-                   Toast.makeText(getApplicationContext(), "Clicked option 2.", Toast.LENGTH_SHORT).show();
-                   intent = new Intent(this, businessappticketagentDashboardActivity.class);
-                   // EditText editText = (EditText) findViewById(R.id.editText);
-                   // String message = editText.getText().toString();
-                   //  intent.putExtra(EXTRA_MESSAGE, message);
-                   startActivity(intent);
-                   break;
-               case 3:
-                   Toast.makeText(getApplicationContext(), "Clicked option 3.", Toast.LENGTH_SHORT).show();
-                   intent = new Intent(this, businessappBrandDashboardActivity.class);
-                   startActivity(intent);
-                   break;
-               case 4:
-                   Toast.makeText(getApplicationContext(), "Clicked option 4.", Toast.LENGTH_SHORT).show();
-                   intent = new Intent(this, businessappBusinessownerDashboardActivity.class);
-                   // EditText editText = (EditText) findViewById(R.id.editText);
-                   // String message = editText.getText().toString();
-                   //  intent.putExtra(EXTRA_MESSAGE, message);
-                   startActivity(intent);
-                   break;
-                   default:
-                        //intent = new Intent(this, customerDashboardActivity.class);
-                       // EditText editText = (EditText) findViewById(R.id.editText);
-                       // String message = editText.getText().toString();
-                       //  intent.putExtra(EXTRA_MESSAGE, message);
-                       //startActivity(intent);
-                       break;
-           }
 
 
         });
@@ -173,6 +161,114 @@ public class login_activity extends AppCompatActivity implements AdapterView.OnI
 //        });
     }
 
+    private void GoToDashboard(){
+        switch (this.loginasOption){
+            case 0:
+                Toast.makeText(getApplicationContext(), "Clicked option 0.", Toast.LENGTH_SHORT).show();
+
+
+                Intent intent = new Intent(this, businessappDriverDashboardActivity.class);
+                // EditText editText = (EditText) findViewById(R.id.editText);
+                // String message = editText.getText().toString();
+                //  intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+
+                break;
+            case 1:
+                Toast.makeText(getApplicationContext(), "Clicked option 1.", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, businessappFleetownerDashboardActivity.class);
+                // EditText editText = (EditText) findViewById(R.id.editText);
+                // String message = editText.getText().toString();
+                //  intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+                break;
+
+            case 2:
+                Toast.makeText(getApplicationContext(), "Clicked option 2.", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, businessappticketagentDashboardActivity.class);
+                // EditText editText = (EditText) findViewById(R.id.editText);
+                // String message = editText.getText().toString();
+                //  intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+                break;
+            case 3:
+                Toast.makeText(getApplicationContext(), "Clicked option 3.", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, businessappBrandDashboardActivity.class);
+                startActivity(intent);
+                break;
+            case 4:
+                Toast.makeText(getApplicationContext(), "Clicked option 4.", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, businessappBusinessownerDashboardActivity.class);
+                // EditText editText = (EditText) findViewById(R.id.editText);
+                // String message = editText.getText().toString();
+                //  intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+                break;
+            default:
+                //intent = new Intent(this, customerDashboardActivity.class);
+                // EditText editText = (EditText) findViewById(R.id.editText);
+                // String message = editText.getText().toString();
+                //  intent.putExtra(EXTRA_MESSAGE, message);
+                //startActivity(intent);
+                break;
+        }
+
+    }
+
+    public void DriverLogin(JsonObject jsonObject){
+
+       // StartDialogue();
+        com.webingate.paysmartbusinessapp.driverapplication.Utils.DataPrepare.get(this).getrestadapter()
+                .ValidateDriver(jsonObject)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<DriverValidateCredentialsResponse>>() {
+                    @Override
+                    public void onCompleted() {
+                        //    DisplayToast("Successfully LoggedIn");
+                     //   StopDialogue();
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        try {
+                            Log.d("OnError ", e.getLocalizedMessage());
+                            DisplayToast("Unable to Login");
+                            StopDialogue();
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onNext(List<DriverValidateCredentialsResponse> responce) {
+                        DriverValidateCredentialsResponse credentialsResponse=responce.get(0);
+                        if(credentialsResponse.getCode()!=null){
+                            DisplayToast(credentialsResponse.getDescription());
+                        }else {
+                            SharedPreferences sharedPref = getSharedPreferences(MyPREFERENCES, MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            editor.putString(DRIVERID, credentialsResponse.getDid());
+                            editor.putString(VEHICLEID, credentialsResponse.getVehicleId());
+                            editor.putString(Phone, mobileNo.getText().toString());
+                            editor.putString(Emailotp, null);
+                            editor.putString(Mobileotp, null);
+                            editor.commit();
+                            ApplicationConstants.mobileNo = mobileNo.getText().toString();
+                            //startActivity(new Intent(this, MainActivity.class));
+                            GoToDashboard();
+                            finish();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //   accessTokenTracker.stopTracking();
+        //   profileTracker.stopTracking();
+    }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         loginasOption = position;
@@ -185,4 +281,25 @@ public class login_activity extends AppCompatActivity implements AdapterView.OnI
 
 
     //endregion
+
+    public void DisplayToast(String text){
+        if(toast!=null){
+            toast.cancel();
+            toast=null;
+
+        }
+        toast=Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+    public void StartDialogue(){
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+    }
+    public void StopDialogue(){
+        if(dialog.isShowing()){
+            dialog.cancel();
+        }
+
+    }
 }
